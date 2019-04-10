52710272ef8fef4ca2a176c8e5923099a24f116b
B0jan93 <samo.bojan933@gmail.com>
2019-04-10 14:51:34 -0700

    add

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,10 @@
 import React from 'react';
 
-import korisnici from '../data/korisnici.json';
-import Utisak from './Utisak';
-
-
-
-const i = 0;
-const mapirano = korisnici.map((obj) => <Utisak key={i} name={obj.name.first} gender={obj.gender}/>)
-
-
-  
-
-
 export function Main(){
     return (
         <main>
             <h2>KORISNICI</h2>
-            {mapirano}
+       
         </main>
     )
 }
