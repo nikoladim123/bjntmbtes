682c87d72d7ca260bb584d87e626fd978451f89b
unknown <nikoladimitrijevic1991@gmail.com>
2019-04-10 23:57:13 +0200

    ccc

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import React, { Component } from 'react';
 
 
 import {Main} from './components/Main'
+import {Brate} from './components/tmbComponent'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
-    
-    
+
+
+
      <Main/>
+     <Brate />
       </div>
     );
   }
---
+++ src/components/tmbComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Brate(){
+    return (
+        <div>
+          <p>ma sta oces bre</p>
+        </div>
+    )
+}
